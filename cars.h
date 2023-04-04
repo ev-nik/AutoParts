000f540ffca69589c77890043fcdcec4d65d0ee2
ev-nik <ev.nik.bondarenko@gmail.com>
2023-04-04 19:24:03 +0300

    Добавлен файл со списками авто

---
+++ cars.h
@@ -0,0 +1,19 @@
+#ifndef CARS_H
+#define CARS_H
+
+#include <string>
+#include <map>
+
+// Список известных автомобилей
+// int - это ключ, std::string - это значение
+std::map<int, std::string> cars =
+{
+    {10, "LADA"      },
+    {20, "TOYOTA"    },
+    {30, "SKODA"     },
+    {40, "VOLKSWAGEN"},
+    {80, "KIA"       }
+};
+
+
+#endif // CARS_H
